ce5ef80cd163a4c0d430784dc16ee768c4d13b8d
jbernal <miw@etsisi.upm.es>
2019-01-18 17:33:28 +0100

    add login & username #4

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 
-import {CashierClosedComponent} from './cashier-closed/cashier-closed.component';
 import {TokensService} from '../core/tokens.service';
+import {UserService} from './shared/user.service';
+import {CashierClosedComponent} from './cashier-closed/cashier-closed.component';
 
 @Component({
   templateUrl: 'home.component.html',
@@ -15,13 +16,16 @@ export class HomeComponent {
   cashierClosed: boolean;
   username: string;
 
-  constructor(private router: Router, private tokensService: TokensService) {
+  constructor(private router: Router, private tokensService: TokensService, private userService: UserService) {
+    this.username = '???';
+    this.userService.sessionUsername().subscribe(
+      user => this.username = user.username
+    );
     this.cashierClosed = true;
     this.cashier();
   }
 
   cashier() {
-    this.username = 'admin';
     this.router.navigate([HomeComponent.URL, CashierClosedComponent.URL]);
   }
 
