ef9fc64ad90d530e7a5ba68b822e7ba6f49ed6fe
Sandra Ortega <sandrita.saan@gmail.com>
2019-03-09 00:59:27 +0100

    Create ArticlesFamilyCreation component #31

---
+++ src/app/home/cashier-opened/articles-family/articles-family-creation.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-articles-family-creation',
+  templateUrl: 'articles-family-creation.component.html'
+})
+export class ArticlesFamilyCreationComponent {
+}
