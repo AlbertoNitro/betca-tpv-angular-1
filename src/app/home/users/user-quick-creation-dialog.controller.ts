c330a2e34c2b254958ece5e582e906c69c38bdcf
Alberto <alberto95.rm@gmail.com>
2019-03-13 20:04:32 +0100

    Create and open quick user creation dialog #56

---
+++ src/app/home/users/user-quick-creation-dialog.controller.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  templateUrl: 'user-quick-creation-dialog.component.html',
+  styleUrls: ['app/home/cashier-opened/shopping-cart/shopping-cart.component.css']
+})
+
+export class UserQuickCreationDialogComponent {
+
+}
