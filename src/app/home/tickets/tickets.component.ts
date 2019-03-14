1d23eef8184a5fedd172e8af42274808b3cc4751
alexph9 <alexph9@hotmail.com>
2019-03-14 23:42:55 +0100

    Create products table for ticket view #58

--- src/app/home/tickets/tickets.component.ts
+++ src/app/home/tickets/tickets.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatTableDataSource} from '@angular/material';
+import {Shopping} from '../cashier-opened/shopping-cart/shopping.model';
 
 export interface Ticket {
   code: string;
@@ -14,9 +16,14 @@ export interface ShoppingCart {
   description: string;
   amount: number;
   discount: number;
-  price: number;
+  retailPrice: number;
   state: string;
   totalPrice: number;
+}
+
+export interface  Status {
+  value: string;
+  viewValue: string;
 }
 
 @Component({
@@ -27,29 +34,54 @@ export interface ShoppingCart {
 
 export class TicketsComponent {
   static URL = 'tickets';
-  shoppingCart: ShoppingCart = {
-    description: 'Blue Jeans',
-    amount: 0,
-    discount: 0,
-    price: 0,
-    state: 'IN_STOCK',
-    totalPrice: 0
-  };
+  shoppingCart: ShoppingCart[] = [
+    {
+      description: 'Blue Jeans',
+      amount: 0,
+      discount: 0,
+      retailPrice: 0,
+      state: 'IN_STOCK',
+      totalPrice: 0,
+    }
+  ] ;
   ticket: Ticket = {
     code: '0',
     date: '05/03/2019 17:15:55',
-    shoppingCart: [this.shoppingCart] ,
+    shoppingCart: this.shoppingCart ,
     cart: 0,
     cash: 0,
     voucher: 0,
     total: 0
   };
+  status: Status[] = [
+    {value: 'NOT-COMMITED', viewValue: 'Not Commited'},
+    {value: 'IN-STOCK', viewValue: 'In Stock'},
+    {value: 'SENDING', viewValue: 'Sending'},
+    {value: 'COMMITED', viewValue: 'Commited'}
+  ];
+  dataSource: MatTableDataSource<ShoppingCart>;
+  displayedColumns = ['id', 'description', 'retailPrice', 'amount', 'discount', 'totalPrice', 'status'];
 
+  constructor() {
+    this.dataSource = new MatTableDataSource<ShoppingCart>(this.shoppingCart);
+  }
   searchTicketById(code: string) {
     // TODO: Create API Call
     console.log('Searching Ticket with id: ', code);
     this.ticket.code = code;
   }
+
+  // TODO: Review if is necessary increment amount or not
+  incrementAmount(shopping: ShoppingCart) {
+    shopping.amount++;
+  }
+
+  decreaseAmount(shopping: ShoppingCart) {
+    if (shopping.amount > 0) {
+      shopping.amount--;
+    }
+  }
+
 }
 
 
