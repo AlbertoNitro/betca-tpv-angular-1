feb8a5e7264f442a04e7494184cfc3d3bf7b33cb
Alberto <alberto95.rm@gmail.com>
2019-03-14 21:25:19 +0100

    Create and open create/update user dialog #63

---
+++ src/app/home/users/user-create-update-dialog.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  templateUrl: 'user-quick-creation-dialog.component.html',
+  styleUrls: ['../../home/cashier-opened/shopping-cart/shopping-cart.component.css']
+})
+
+export class UserCreateUpdateDialog implements OnInit {
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+  create() {
+    return null;
+  }
+}
