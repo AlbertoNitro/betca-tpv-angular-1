1314821558911c5d27f8baa6adca2b7c5568283a
fatandaz <fatandaz@gmail.com>
2019-03-14 23:00:20 +0100

    Grafical order interface issue #61

--- src/app/home/order/order.component.ts
+++ src/app/home/order/order.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {Order} from './order.model';
 import {User} from '../users/user.model';
 import {OrderService} from './order.service';
+import {MatDialog} from '@angular/material';
 
 @Component({
   selector: 'app-order',
@@ -9,15 +10,26 @@ import {OrderService} from './order.service';
 })
 export class OrderComponent {
   static URL = 'orders';
-
+  onlyClosingDate = false;
+  order: { descriptionOrders: null; descriptionArticles: null };
   data: Order[];
   title = 'Order management';
-  columns = ['description'];
+  columns = ['Description orders', 'Description articles', 'Required amount', 'Final amount', 'Opening date', 'Closing date'];
 
-  constructor(private orderService: OrderService) {
+  constructor(private orderService: OrderService, private  dialog: MatDialog) {
+    this.order = {descriptionOrders: null, descriptionArticles: null };
+    this.data = null;
+  }
+
+  search() {
+    // TODO implement search with fields
     this.orderService.readAll().subscribe(
       data => this.data = data
     );
+  }
+
+  resetSearch() {
+    this.order = {descriptionOrders: null, descriptionArticles: null };
   }
 
   create() {
