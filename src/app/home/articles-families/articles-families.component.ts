028bf4a01049458d14fbf17aeead63bad3bb6c40
Bruno Martin <=>
2019-03-16 10:20:59 +0100

    Initial commit #71

--- src/app/home/articles-families/articles-families.component.ts
+++ src/app/home/articles-families/articles-families.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {ArticleFamily} from './articles-families';
+import {ArticleFamily} from './articles-families.model';
 
 @Component({
   selector: 'app-articles-families',
