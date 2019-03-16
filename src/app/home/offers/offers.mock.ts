12361e7e726cc4fe2265190d957a3662005f1165
Jose Lorenzo <jolorenzom@gmail.com>
2019-03-16 10:35:51 +0100

    Refactor Mock and views. #70

--- src/app/home/offers/offers.mock.ts
+++ src/app/home/offers/offers.mock.ts
@@ -19,5 +19,77 @@ export const OFFERS: Offer[] = [
         percentage: 2
       }
     ]
-  }
+  },
+  {
+    id: '2',
+    name: 'Prueba2',
+    endDate: '03-12-2019',
+    articleLine: [
+      {
+        idArticle: '1',
+        percentage: 12
+      },
+      {
+        idArticle: '3',
+        percentage: 2
+      }
+    ]
+  },
+  {
+    id: '3',
+    name: 'Prueba3',
+    endDate: '12-07-2020',
+    articleLine: [
+      {
+        idArticle: '5',
+        percentage: 2
+      },
+      {
+        idArticle: '2',
+        percentage: 34
+      },
+      {
+        idArticle: '3',
+        percentage: 26
+      }
+    ]
+  },
+  {
+    id: '4',
+    name: 'Prueba4',
+    endDate: '30-11-2019',
+    articleLine: [
+      {
+        idArticle: '5',
+        percentage: 2
+      }
+    ]
+  },
+  {
+    id: '4',
+    name: 'Prueba4',
+    endDate: '29-04-2019',
+    articleLine: [
+      {
+        idArticle: '5',
+        percentage: 2
+      },
+      {
+        idArticle: '2',
+        percentage: 34
+      },
+      {
+        idArticle: '3',
+        percentage: 26
+      },
+      {
+        idArticle: '15',
+        percentage: 4
+      },
+      {
+        idArticle: '1',
+        percentage: 12
+      }
+    ]
+  },
 ];
