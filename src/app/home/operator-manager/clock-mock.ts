3decbe0cf4f0e5eb39eb79b5f2d782ef966f72af
vpaniego <vanesa.paniegos@alumnos.upm.es>
2019-03-16 18:33:24 +0100

    issue #22 Mock clock Operate-Manager

---
+++ src/app/home/operator-manager/clock-mock.ts
@@ -0,0 +1,21 @@
+import {Offer} from '../offers/offer.model';
+import {Clock} from './clock';
+
+export const CLOCKS: Clock[] = [
+  {
+    id: '1',
+    employee: 'Berenguela Nuñez',
+    date: '03-07-2019',
+    in: '08:00',
+    out: '17:00',
+    total: '9'
+  },
+  {
+    id: '1',
+    employee: 'Alonso Nuñez',
+    date: '03-07-2019',
+    in: '09:00',
+    out: '18:00',
+    total: '9'
+  }
+];
