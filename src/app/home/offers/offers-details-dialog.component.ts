90b4e4ba7668ecfe3d1965e775229cca109a8a53
Jose Lorenzo <jolorenzom@gmail.com>
2019-03-16 09:30:02 +0100

    Create Offer Mock and refactor. #70

--- src/app/home/offers/offers-details-dialog.component.ts
+++ src/app/home/offers/offers-details-dialog.component.ts
@@ -1,24 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
   selector: 'app-offers-details-dialog',
   templateUrl: './offers-details-dialog.component.html'
 })
-export class OffersDetailsDialogComponent {
+export class OffersDetailsDialogComponent implements OnInit {
   title = 'Articles list';
-  columns = ['Id', 'Description', 'Percentage'];
-  data: object[];
+  columns = ['Code', 'Percentage'];
+  data: object;
 
-  constructor() {
-    this.data = [
-      { 'Id': 1, 'Description': 'Description Article1', 'Percentage': '5' },
-      { 'Id': 2, 'Description': 'Description Article2', 'Percentage': '7' },
-      { 'Id': 3, 'Description': 'Description Article3', 'Percentage': '3' },
-      { 'Id': 4, 'Description': 'Description Article4', 'Percentage': '5' },
-      { 'Id': 5, 'Description': 'Description Article5', 'Percentage': '3' },
-      { 'Id': 6, 'Description': 'Description Article6', 'Percentage': '14' },
-      { 'Id': 7, 'Description': 'Description Article7', 'Percentage': '31' },
-    ];
+  constructor(@Inject(MAT_DIALOG_DATA) offer: object) {
+    this.data = offer;
+  }
+
+  ngOnInit(): void {
+    console.log(this.data, '<<<<<<<< DATA DETAILS');
   }
 
 }
