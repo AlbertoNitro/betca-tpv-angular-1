cc7b115594f1ec8af4576e60aabfbcb06cfde47e
pmoreno <pmorenopina@gmail.com>
2019-03-17 21:30:08 +0100

    Added model and crud template for budget component #28

--- src/app/home/budgets/budgets.component.ts
+++ src/app/home/budgets/budgets.component.ts
@@ -1,12 +1,29 @@
 import {Component} from '@angular/core';
 
+import {Budget} from './budgets.model';
+
 @Component({
   templateUrl: 'budgets.component.html'
 })
 export class BudgetsComponent {
   static URL = 'budgets';
 
+  budget: {code: null};
+
+  data: Budget[];
+  title = 'Budget management';
+  columns = ['Code'];
+
   constructor() {
+    this.budget = {code: null};
+  }
+
+  search() {
+    // TODO implement search
+  }
+
+  resetSearch() {
+    this.budget = {code: null};
   }
 
 }
