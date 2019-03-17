390e61c9099f0a583286afdb8ddbf6a9a402b446
Bruno Martin <=>
2019-03-17 17:18:07 +0100

    dialog created and working #87

---
+++ src/app/home/articles-families/articles-families-create-dialog.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-articles-families-create-dialog',
+  templateUrl: './articles-families-create-dialog.component.html'
+})
+export class ArticlesFamiliesCreateDialogComponent {
+  constructor() {
+  }
+}
