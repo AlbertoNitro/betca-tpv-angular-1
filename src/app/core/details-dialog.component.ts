1cd3f54fa80488ee5a156230e376ef883d23c308
iraticasi <iraticasi@gmail.com>
2019-03-19 19:29:42 +0100

    providers details with generic details dialog component #91

---
+++ src/app/core/details-dialog.component.ts
@@ -0,0 +1,20 @@
+import {Component, Inject, Input} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material';
+
+@Component({
+  templateUrl: 'details-dialog.component.html',
+  styleUrls: ['details-dialog.component.css']
+
+})
+export class DetailsDialogComponent {
+  
+  object: any;
+  title: string;
+  properties: string[];
+
+  constructor(@Inject(MAT_DIALOG_DATA) data: any){
+    this.title = data.title;
+    this.properties = data.properties;
+    this.object = data.object;
+  }
+}
