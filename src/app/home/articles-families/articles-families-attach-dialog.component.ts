e5c3b83e10140faff039990bb125bfd61a2d63e0
Bruno Martin <=>
2019-03-24 17:34:43 +0100

    added articles to be selected #138

--- src/app/home/articles-families/articles-families-attach-dialog.component.ts
+++ src/app/home/articles-families/articles-families-attach-dialog.component.ts
@@ -5,22 +5,31 @@ import {ArticleFamilyService} from './articles-families.service';
 import {FamilyTypes} from './family-types.model';
 import {FamilyType} from './family-type.model';
 import {ArticleMinimum} from '../shared/article-minimum.model';
+import {ArticleService} from '../shared/article.service';
+import {ArticleFamily} from './articles-families.model';
 
 @Component({
   selector: 'app-articles-families-attach-dialog',
   templateUrl: './articles-families-attach-dialog.component.html'
 })
 export class ArticlesFamiliesAttachDialogComponent {
+  articlesFamily: ArticleFamily[];
+  articleFamilySelected: ArticleFamily;
   articlesMinimum: ArticleMinimum[];
   articleMinimumSelected: ArticleMinimum;
   familyTypes: FamilyTypes = new FamilyTypes();
   familyTypeSelected: FamilyType;
 
-  constructor(private articleFamilyService: ArticleFamilyService, @Inject(MAT_DIALOG_DATA) private parentFamily: ArticleFamilyMinimum) {
+  constructor(private articleFamilyService: ArticleFamilyService, @Inject(MAT_DIALOG_DATA) private parentFamily: ArticleFamilyMinimum,
+              private articleService: ArticleService) {
   }
 
   whenSelected() {
+    if (this.familyTypeSelected === this.familyTypes.families[0]) {
+      this.articleService.readAllArticleMinimum().subscribe(data => this.articlesMinimum = data);
+    } else {
 
+    }
   }
 
   isValid() {
