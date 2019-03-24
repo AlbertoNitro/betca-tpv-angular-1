add710c4779d1e8c6c8df1cb2749ab8de5d42765
Bruno Martin <=>
2019-03-24 17:15:55 +0100

    updated attach dialog mok #137

--- src/app/home/articles-families/articles-families-attach-dialog.component.ts
+++ src/app/home/articles-families/articles-families-attach-dialog.component.ts
@@ -2,13 +2,31 @@ import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material';
 import {ArticleFamilyMinimum} from './articles-families-minimum.model';
 import {ArticleFamilyService} from './articles-families.service';
+import {FamilyTypes} from './family-types.model';
+import {FamilyType} from './family-type.model';
+import {ArticleMinimum} from '../shared/article-minimum.model';
 
 @Component({
   selector: 'app-articles-families-attach-dialog',
   templateUrl: './articles-families-attach-dialog.component.html'
 })
 export class ArticlesFamiliesAttachDialogComponent {
-
+  articlesMinimum: ArticleMinimum[];
+  articleMinimumSelected: ArticleMinimum;
+  familyTypes: FamilyTypes = new FamilyTypes();
+  familyTypeSelected: FamilyType;
   constructor(private articleFamilyService: ArticleFamilyService, @Inject(MAT_DIALOG_DATA) private parentFamily: ArticleFamilyMinimum) {
+  }
+
+  whenSelected() {
+
+  }
+
+  isValid() {
+    return false;
+  }
+
+  create() {
+
   }
 }
