20037935c0440b116e32be9efa1984d943ce8324
Degor <x.sotomayor@alumnos.upm.es>
2019-03-26 10:11:43 +0100

    Add Articles Family Views #125

--- src/app/home/cashier-opened/articles-family/articles-family-view.component.ts
+++ src/app/home/cashier-opened/articles-family/articles-family-view.component.ts
@@ -2,8 +2,24 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-articles-family-view',
-  templateUrl: 'articles-family-view.component.html'
+  templateUrl: 'articles-family-view.component.html',
+  styleUrls: ['articles-family-view.component.css']
 })
+
 export class ArticlesFamilyViewComponent {
 
+  family_description = '';
+  route_family_element = '';
+
+  familyArticle() {
+    console.log('content family article');
+  }
+
+  familyComposite() {
+    console.log('content family composite');
+  }
+
+  familySize() {
+    console.log('content family size');
+  }
 }
---
+++ src/app/home/cashier-opened/articles-family/family-composite-view.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-family-composite-view',
+  templateUrl: 'family-composite-view.component.html',
+  styleUrls: ['articles-family-view.component.css']
+})
+
+export class FamilyCompositeViewComponent {
+
+}
