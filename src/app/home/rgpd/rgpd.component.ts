795c02da25795a21b8539ca4a6f5bba87ded5f95
Ramon Roca <r.roca@alumnos.upm.es>
2019-03-27 11:05:36 +0100

    Add functionality to rgpd component and view. #156

--- src/app/home/rgpd/rgpd.component.ts
+++ src/app/home/rgpd/rgpd.component.ts
@@ -1,16 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 
 @Component({
   selector: 'app-rgpd',
   templateUrl: './rgpd.component.html'
 })
-export class RGPDComponent {
+export class RGPDComponent implements OnInit {
   static URL = 'rgpd';
   title = 'RGPD management';
 
+  showFileUpload: boolean;
+  agreementType: string;
+  documentResult;
 
   constructor() {
+    this.agreementType = '1';
+    this.showFileUpload = false;
+    console.log('Constructor Executed!');
   }
 
+  onAgreementSelected(event: Event) {
+    const input = event.target;
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.documentResult = reader.result;
+      this.documentResult = window.btoa(this.documentResult);
+    };
+    reader.readAsBinaryString((<HTMLInputElement>input).files[0]);
+    console.log('Create service to call API Rest with: ' + this.agreementType + ' and agreement ' + this.documentResult);
+  }
+
+  printAgreement() {
+    this.showFileUpload = true;
+    console.log('Create service to call API Rest with: ' + this.agreementType + ' and get Agreement to sign.');
+  }
+
+  ngOnInit() {
+    this.agreementType = '1';
+    this.showFileUpload = false;
+    console.log('On Init Executed!');
+  }
 }
