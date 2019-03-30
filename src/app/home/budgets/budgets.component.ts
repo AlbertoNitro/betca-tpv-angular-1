4ef942b2b8279f846f17073ff6391f84f76ba259
pmoreno <pmorenopina@gmail.com>
2019-03-30 19:51:08 +0100

    Adecuated searchAll budget #29

--- src/app/home/budgets/budgets.component.ts
+++ src/app/home/budgets/budgets.component.ts
@@ -9,29 +9,24 @@ import {BudgetService} from './budget.service';
 export class BudgetsComponent {
   static URL = 'budgets';
 
-  budget: {code: null};
+  budget: {id: null};
 
   data: Budget[];
   title = 'Budget management';
-  columns = ['code'];
+  columns = ['id'];
 
   constructor(private budgetService: BudgetService) {
-    this.budget = {code: null};
+    this.budget = {id: null};
   }
 
   search() {
-    // TODO implement real search
-    /*this.budgetService.readAll().subscribe(
+    this.budgetService.readAll().subscribe(
       data => this.data = data
-    );*/
-    this.data = [
-      {'code': '11111111', 'shoppingCart': []}, {'code': '22222222', 'shoppingCart': []},
-      {'code': '33333333', 'shoppingCart': []}, {'code': '44444444', 'shoppingCart': []}
-    ];
+    );
   }
 
   resetSearch() {
-    this.budget = {code: null};
+    this.budget = {id: null};
   }
 
 }
