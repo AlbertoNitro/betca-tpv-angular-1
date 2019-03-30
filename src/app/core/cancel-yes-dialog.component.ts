84ffb869e6dbe3b6699c6daf0ab5a764def99392
Angélica Guamán Albarracín <angelica.guaman.albarracin@gmail.com>
2019-03-30 23:26:31 +0100

    modified cancelYesDialogComponent allows dialog configuration (messages) #191

--- src/app/core/cancel-yes-dialog.component.ts
+++ src/app/core/cancel-yes-dialog.component.ts
@@ -1,8 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
   templateUrl: 'cancel-yes-dialog.component.html',
   styleUrls: ['./dialog.component.css']
 })
 export class CancelYesDialogComponent {
+
+  message: string;
+  question: string;
+
+  constructor(@Inject(MAT_DIALOG_DATA) data: any) {
+    this.message = data.message;
+    this.question = data.question;
+  }
 }
