414793c08bbbffde1d7024407fe602ba527a317e
Ramon Roca <r.roca@alumnos.upm.es>
2019-03-30 17:32:27 +0100

    Added API calls. #182

--- src/app/home/rgpd/rgpd.service.ts
+++ src/app/home/rgpd/rgpd.service.ts
@@ -8,11 +8,28 @@ import {Rgpd} from './rgpd.model';
 export class RgpdService {
   static URL_AGREEMENT = '/rgpd/agreement';
 
+  static USER_AGREEMENT = '/rgpd/useragreement';
+
   constructor(private httpService: HttpService) {
   }
 
   createPrintableAgreement(type: Rgpd): Observable<Rgpd> {
-    console.log('Call Service ' + RgpdService.URL_AGREEMENT );
+    console.log('Call Service print agreement ' + RgpdService.URL_AGREEMENT);
     return this.httpService.post(RgpdService.URL_AGREEMENT, type);
+  }
+
+  getUserAgreement(): Observable<Rgpd> {
+    console.log('Call Service get user agreement ' + RgpdService.USER_AGREEMENT);
+    return this.httpService.get(RgpdService.USER_AGREEMENT);
+  }
+
+  createUserAgreement(rgpd: Rgpd): Observable<Rgpd> {
+    console.log('Call Service accept user agreement ' + RgpdService.USER_AGREEMENT);
+    return this.httpService.post(RgpdService.USER_AGREEMENT, rgpd);
+  }
+
+  deleteUserAgreement(): Observable<Rgpd> {
+    console.log('Call Service revoke user agreement ' + RgpdService.USER_AGREEMENT);
+    return this.httpService.delete(RgpdService.USER_AGREEMENT);
   }
 }
