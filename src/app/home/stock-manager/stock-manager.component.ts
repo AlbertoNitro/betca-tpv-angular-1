8c7441df64ab2e5d7c08352d732a91cf7512b7b8
ATorrecilla <a.torrecilla@alumnos.upm.es>
2019-05-19 11:54:34 +0200

    Add stock-manager minimum issue #214

--- src/app/home/stock-manager/stock-manager.component.ts
+++ src/app/home/stock-manager/stock-manager.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ArticleDetailModel} from '../shared/article-detail-model';
 
 @Component({
   selector: 'app-stock-manager',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockManagerComponent implements OnInit {
   static URL = 'stock-manager';
-  constructor() { }
+  stockManagerForm: FormGroup;
+  listTitle = 'Stock manager';
+  columns = ['code', 'description', 'retailPrice', 'stock'];
+  articleList: ArticleDetailModel[];
+
+  constructor(private formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
+    this.stockManagerForm = this.formBuilder.group({
+        minimum: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      }
+    );
+  }
+
+  searchStockMin() {
+    console.log('stockManagerForm ' + this.stockManagerForm.get('minimum').value);
+    const limit = this.stockManagerForm.get('minimum').value;
+    console.log('limit ' + limit);
+  }
+
+  resetSearchStock() {
+    console.log('reset search Stock');
+    this.stockManagerForm.reset();
   }
 
 }
