c3337eaeba3995a14fff810f3bd35a4602e8f399
ATorrecilla <a.torrecilla@alumnos.upm.es>
2019-05-29 19:18:20 +0200

    Add stock-manager minimum issue #214

--- src/app/home/stock-manager/stock-manager.component.ts
+++ src/app/home/stock-manager/stock-manager.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ArticleDetailModel} from '../shared/article-detail-model';
+import {StockManagerService} from './stock-manager.service';
 
 @Component({
   selector: 'app-stock-manager',
@@ -7,12 +10,34 @@ import {Component, OnInit} from '@angular/core';
 })
 export class StockManagerComponent implements OnInit {
   static URL = 'stock-manager';
+  stockManagerForm: FormGroup;
+  listTitle = 'Stock manager';
   columns = ['code', 'description', 'retailPrice', 'stock'];
+  articleList: ArticleDetailModel[];
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder, private stockManagerService: StockManagerService) {
   }
 
   ngOnInit() {
+    this.stockManagerForm = this.formBuilder.group({
+        minimum: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      }
+    );
   }
 
+  searchStockMin() {
+    console.log('stockManagerForm ' + this.stockManagerForm.get('minimum').value);
+    const limit = 1 + this.stockManagerForm.get('minimum').value;
+    this.stockManagerService.getArticlesMinimum(limit).subscribe(
+      articleList => {
+        this.articleList = articleList;
+        console.log('art list ' + this.articleList.length);
+      }
+    );
+  }
+
+  resetSearchStock() {
+    console.log('reset search Stock');
+    this.stockManagerForm.reset();
+  }
 }
