5f6fe0ee26adc0e20dcfd74f96eeb88890611f73
Cesar Gayo Bravo <cesar.gayo.bravo@alumnos.upm.es>
2019-05-31 15:05:42 +0200

    Add min max values in negative-invoice-dialog validation #220

--- src/app/home/shared/negative-invoice-dialog.component.ts
+++ src/app/home/shared/negative-invoice-dialog.component.ts
@@ -2,12 +2,27 @@ import {Component, Inject, Input, Output} from '@angular/core';
 import {MAT_DIALOG_DATA, MatFormFieldControl} from '@angular/material';
 import {InvoiceUpdateService} from '../invoice/invoice-update.service';
 import {InvoiceUpdateModel} from './invoice-update.model';
+import {FormControl, ValidatorFn, Validators} from '@angular/forms';
 
 @Component({
   templateUrl: 'negative-invoice-dialog.component.html',
   styleUrls: ['./negative-invoice-dialog.component.css'],
 })
 export class NegativeInvoiceDialogComponent  {
+
+  constructor(@Inject(MAT_DIALOG_DATA) data: any, private invoiceUpdateService: InvoiceUpdateService) {
+    this.message = data.message;
+    this.id = data.invoice.id;
+    this.data = data.invoice;
+    this.positiveValue = 0;
+    this.invoiceUpdateService.look4PosibleTotal(this.id).subscribe(
+      posibletotal => {
+        if (posibletotal >= 0) {
+          this.positiveValue = posibletotal;
+        } else {this.positiveValue = 0; }
+      }
+      );
+  }
 
   @Input() message: string;
   @Input() positiveValue: number;
@@ -18,16 +33,6 @@ export class NegativeInvoiceDialogComponent  {
   baseTax: string;
   tax: string;
   negativeValue: number;
-
-  constructor(@Inject(MAT_DIALOG_DATA) data: any, private invoiceUpdateService: InvoiceUpdateService) {
-    this.message = data.message;
-    this.id = data.invoice.id;
-    this.data = data.invoice;
-    this.invoiceUpdateService.look4PosibleTotal(this.id).subscribe(
-      posibletotal =>
-        this.positiveValue = posibletotal
-    );
-  }
   generateNegativeInvoice() {
     const negativeInvoice: InvoiceUpdateModel = this.data;
     negativeInvoice.referencesPositiveInvoice = this.id;
