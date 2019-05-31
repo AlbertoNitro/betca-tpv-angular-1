8e1197d4294054ddc2a3d5686baf6a07b8f25bbf
Cesar Gayo Bravo <cesar.gayo.bravo@alumnos.upm.es>
2019-05-31 18:03:39 +0200

    Add tax info to invoice-update dialog, asign newTax to invoiceUpdateDto issue #223

--- src/app/home/invoice/negative-invoice-dialog.component.ts
+++ src/app/home/invoice/negative-invoice-dialog.component.ts
@@ -1,18 +1,33 @@
 import {Component, Inject, Input, Output} from '@angular/core';
 import {MAT_DIALOG_DATA, MatFormFieldControl} from '@angular/material';
-import {InvoiceUpdateService} from '../invoice/invoice-update.service';
-import {InvoiceUpdateModel} from './invoice-update.model';
-import {FormControl, ValidatorFn, Validators} from '@angular/forms';
+import {InvoiceUpdateService} from './invoice-update.service';
+import {InvoiceUpdateModel} from '../shared/invoice-update.model';
 
 @Component({
   templateUrl: 'negative-invoice-dialog.component.html',
   styleUrls: ['./negative-invoice-dialog.component.css'],
 })
 export class NegativeInvoiceDialogComponent  {
+  @Input() message: string;
+  @Input() positiveValue: number;
+  @Input() totalTax: number;
+  @Output() newTax: number;
+  id: string;
+  data: InvoiceUpdateModel;
+  creationDate: string;
+  baseTax: string;
+  vatTax: string;
+  tax: string;
+  maxVATTax: number;
+  negativeValue: number;
 
   constructor(@Inject(MAT_DIALOG_DATA) data: any, private invoiceUpdateService: InvoiceUpdateService) {
     this.message = data.message;
     this.id = data.invoice.id;
+    this.baseTax = data.invoice.baseTax;
+    this.vatTax = data.invoice.tax;
+    this.totalTax = Number(this.baseTax) + Number(this.vatTax);
+    this.maxVATTax = data.invoice.tax;
     this.data = data.invoice;
     this.positiveValue = 0;
     this.invoiceUpdateService.look4PosibleTotal(this.id).subscribe(
@@ -24,22 +39,17 @@ export class NegativeInvoiceDialogComponent  {
       );
   }
 
-  @Input() message: string;
-  @Input() positiveValue: number;
-  @Input() totalTax: number;
-  id: string;
-  data: InvoiceUpdateModel;
-  creationDate: string;
-  baseTax: string;
-  tax: string;
-  negativeValue: number;
+
   generateNegativeInvoice() {
     const negativeInvoice: InvoiceUpdateModel = this.data;
     negativeInvoice.referencesPositiveInvoice = this.id;
+    negativeInvoice.tax = this.newTax;
     negativeInvoice.negative = this.negativeValue;
-    this.invoiceUpdateService.generateNegative(negativeInvoice).subscribe();
+    if (this.positiveValue > 0) {
+      this.invoiceUpdateService.generateNegative(negativeInvoice).subscribe();
+    }
   }
-  setPosibleTotal(id: any) {
+  setPosibleTotal() {
     this.negativeValue = this.positiveValue;
   }
 }
